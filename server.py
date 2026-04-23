c44727a625138a70d49d74ada413d678886d7933
admin <admin@192.168.1.3>
2026-04-23 23:13:36 +0800

    Place signature through stamp edge

--- server.py
+++ server.py
@@ -3219,8 +3219,8 @@ def build_invoice_html(record, asset_mode="web"):
       }}
       .accountant-signature {{
         position: absolute;
-        left: 150px;
-        bottom: 22px;
+        left: 104px;
+        bottom: 0;
         width: 290px;
         z-index: 2;
       }}
